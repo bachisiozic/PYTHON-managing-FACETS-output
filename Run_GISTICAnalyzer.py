1b4bf890fbdc60143e0afaa887c778caaa34b793
Bachisio Ziccheddu <65035163+bachisiozic@users.noreply.github.com>
2024-02-26 15:43:35 -0500

    Update Run_GISTICAnalyzer.py

--- Run_GISTICAnalyzer.py
+++ Run_GISTICAnalyzer.py
@@ -1,11 +1,12 @@
 #### . . . Running the GISTIC analyzer
 if __name__ == "__main__":
-    analyzer = GISTICAnalyzer("/Users/bxz262/Library/CloudStorage/GoogleDrive-bxz262@miami.edu/My Drive/Bachisio/Documents/PhD_PiBS_CAB/Courses/Courses_Spring2024/PIB_706_Informatics_for_the_Biosciences/Assignments/final_project_proposal/WORK", ".txt")
-    header, _, data = analyzer.file_collapsing("collapsed_data.tsv")
+    analyzer = GISTICAnalyzer("/Users/bxz262/Library/CloudStorage/Box-Box/Ziccheddu/final_project_proposal/WORK", ".txt")
+    header, _, data = analyzer.file_collapsing("/Users/bxz262/Library/CloudStorage/Box-Box/Ziccheddu/final_project_proposal/WORK/output/collapsed_data.tsv")
     print(header)
     print(data)
 
-    result_df = analyzer.get_alterated_GISTIC_peaks("Genomic_Coordinates_GISTICpeaks.tsv", "collapsed_data.tsv", "GISTICmatrix.tsv")
+    result_df = analyzer.get_alterated_GISTIC_peaks("Genomic_Coordinates_GISTICpeaks.tsv", "/Users/bxz262/Library/CloudStorage/Box-Box/Ziccheddu/final_project_proposal/WORK/output/collapsed_data.tsv", "/Users/bxz262/Library/CloudStorage/Box-Box/Ziccheddu/final_project_proposal/WORK/output/GISTICmatrix.tsv")
     print(result_df.head())
 
-    analyzer.create_heatmap(result_df, "heatmap.pdf")
+    analyzer.create_heatmap(result_df, "/Users/bxz262/Library/CloudStorage/Box-Box/Ziccheddu/final_project_proposal/WORK/output/heatmap.pdf")
+
